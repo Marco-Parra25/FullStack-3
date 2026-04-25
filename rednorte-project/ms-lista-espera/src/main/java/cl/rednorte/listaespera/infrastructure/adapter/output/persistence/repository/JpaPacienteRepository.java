ef1e17acffe8b18a641342bdd8dc21e4c34fee44
hector peña <hec.penac@duocuc.cl>
2026-04-24 21:43:25 -0400

    chore: limpieza definitiva - solo queda el microservicio

--- rednorte-project/ms-lista-espera/src/main/java/cl/rednorte/listaespera/infrastructure/adapter/output/persistence/repository/JpaPacienteRepository.java
+++
@@ -1,7 +0,0 @@
-package cl.rednorte.listaespera.infrastructure.adapter.output.persistence.repository;
-
-import cl.rednorte.listaespera.infrastructure.adapter.output.persistence.entity.PacienteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaPacienteRepository extends JpaRepository<PacienteEntity, Long> {
-}
