353b6a0b9452a72a90767b3e7a08ac97eaf5423b
hector peña <hec.penac@duocuc.cl>
2026-04-24 21:30:35 -0400

    Correcciones en test msReasignacion y pom

--- ms-reasignacion/src/test/java/org/msreasignacion/MsReasignacionApplicationTest.java
+++ ms-reasignacion/src/test/java/org/msreasignacion/MsReasignacionApplicationTest.java
@@ -2,13 +2,23 @@ package org.msreasignacion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driverClassName=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.flyway.enabled=false",
+        "spring.jpa.hibernate.ddl-auto=update",
+        "spring.kafka.enabled=false"
+})
+@ActiveProfiles("test")
 class MsReasignacionApplicationTest {
 
     @Test
     void contextLoads() {
-        // Si este test pasa, significa que Spring Boot logró levantar
-        // toda la configuración y conectarse a la base de datos sin caerse.
+        // Al definir las properties aquí arriba, obligamos a Spring
+        // a usar H2 y apagar Flyway, sin importar lo que diga el application.yml
     }
 }
