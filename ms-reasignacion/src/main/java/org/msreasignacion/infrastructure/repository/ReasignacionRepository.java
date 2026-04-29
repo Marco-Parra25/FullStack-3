5f858fd0ef54a36ae8e6b5e257efc98e4aa89d5d
Hector <hec.penac@duocuc.cl>
2026-04-29 13:05:00 -0400

    se crea dto, actualiza el usecase(service), modificaciones generales

--- ms-reasignacion/src/main/java/org/msreasignacion/infrastructure/repository/ReasignacionRepository.java
+++ ms-reasignacion/src/main/java/org/msreasignacion/infrastructure/repository/ReasignacionRepository.java
@@ -1,11 +1,12 @@
 package org.msreasignacion.infrastructure.repository;
 
 import org.msreasignacion.domain.model.Reasignacion;
+import org.msreasignacion.infrastructure.adapter.out.db.ReasignacionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ReasignacionRepository extends JpaRepository<Reasignacion, UUID> {
+public interface ReasignacionRepository extends JpaRepository<ReasignacionEntity, UUID> {
 }
