3816206db807efe68d06f636fcc3bd3b98c85965
Hector <hec.penac@duocuc.cl>
2026-04-29 20:19:51 -0400

    clean: dejar unicamente el microservicio de notificaciones

--- ms-notificaciones/src/main/java/org/msnotificaciones/domain/event/CupoAsignadoEvent.java
+++
@@ -1,8 +0,0 @@
-package org.msnotificaciones.domain.event;
-
-public record CupoAsignadoEvent(
-        String pacienteRut,
-        String telefono,
-        String email,
-        String especialidad
-) {}
--- ms-notificaciones/src/main/java/org/msnotificaciones/domain/port/out/NotificacionPort.java
+++
@@ -1,6 +0,0 @@
-package org.msnotificaciones.domain.port.out;
-
-public interface NotificacionPort {
-    void enviarEmail(String destinatario, String mensaje, String rut, String especialidad);
-    void enviarSMS(String telefono, String mensaje, String rut, String especialidad);
-}
