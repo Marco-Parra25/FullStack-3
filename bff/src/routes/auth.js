617588de6cbbf82baaea7d03c097509d33f4e2fa
dxminique <do.cofre@duocuc.cl>
2026-05-15 20:58:25 -0400

    Create auth.js

---
+++ bff/src/routes/auth.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const axios = require('axios')
+const qs = require('querystring')
+
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const response = await axios.post(
+      `${process.env.KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
+      qs.stringify({
+        grant_type: 'password',
+        client_id: process.env.KEYCLOAK_CLIENT_ID,
+        client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+        username,
+        password
+      }),
+      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+    )
+    res.json({ token: response.data.access_token })
+  } catch (error) {
+    res.status(401).json({ error: 'Credenciales inválidas' })
+  }
+})
+
+module.exports = router
